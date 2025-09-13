7050b0ee3601d47c00b6b2cf0c73f2ed982780e9
Qmqmmyay <your.email@example.com>
2025-09-13 17:09:37 +0700

    Update base URL for GitHub Pages

--- 3. In-depth Understanding of Advanced React Functionality/Lab: E-Commerce Data Rendering using Redux Toolkit/ecommerce_rtk/vite.config.js
+++ 3. In-depth Understanding of Advanced React Functionality/Lab: E-Commerce Data Rendering using Redux Toolkit/ecommerce_rtk/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ecommerce_rtk/', // Add base URL for GitHub Pages
-})
+  base: '/Developing-Front-end-Apps-with-React/'
+}),
