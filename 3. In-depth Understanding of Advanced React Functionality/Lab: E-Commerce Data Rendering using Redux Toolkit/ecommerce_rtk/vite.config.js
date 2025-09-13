4e5842ba11dd143bd66d1c8936e78afe9163a13f
Qmqmmyay <your.email@example.com>
2025-09-13 17:11:02 +0700

    Fix base URL path and remove extra comma

--- 3. In-depth Understanding of Advanced React Functionality/Lab: E-Commerce Data Rendering using Redux Toolkit/ecommerce_rtk/vite.config.js
+++ 3. In-depth Understanding of Advanced React Functionality/Lab: E-Commerce Data Rendering using Redux Toolkit/ecommerce_rtk/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Developing-Front-end-Apps-with-React/'
-}),
+  base: '/Developing-Front-end-Apps-with-React/3.%20In-depth%20Understanding%20of%20Advanced%20React%20Functionality/Lab:%20E-Commerce%20Data%20Rendering%20using%20Redux%20Toolkit/ecommerce_rtk/'
+})
