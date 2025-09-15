78640d116358f00f8713bd7622212a714d188c6f
Qmqmmyay <your.email@example.com>
2025-09-15 22:05:08 +0700

    Add event_planner and todo_list project files

---
+++ 2. Understanding Function components with array and DOM manipulation/Lab: Function Component Event Planner Landing Page/event_planner/src/Components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+    <footer className="footer">
+        <p>&copy; 2023 Event Planner. All rights reserved.</p>
+    </footer>
+    )};
+export default Footer
+
