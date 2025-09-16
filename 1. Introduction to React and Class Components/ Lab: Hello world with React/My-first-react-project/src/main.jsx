d76ce8361d7cf6ae2a682fb4612ebf65c0424b5a
Qmqmmyay <your.email@example.com>
2025-09-16 13:44:08 +0700

    Updates

--- 1. Introduction to React and Class Components/ Lab: Hello world with React/My-first-react-project/src/main.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App1, App2, App3 } from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App3 />);
