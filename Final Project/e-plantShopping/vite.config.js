03a890be37deba1a0a54b672299eb77818aa8936
Qmqmmyay <your.email@example.com>
2025-09-16 14:04:44 +0700

    Fix path issues in GitHub Actions workflow and update base paths

--- Final Project/e-plantShopping/vite.config.js
+++ Final Project/e-plantShopping/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Developing-Front-end-Apps-with-React/Final Project/e-plantShopping/",
+  base: "/Developing-Front-end-Apps-with-React/e-plantShopping/",
   plugins: [react()],
 })
