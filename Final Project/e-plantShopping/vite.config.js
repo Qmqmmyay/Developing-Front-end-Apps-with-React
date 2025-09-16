a5d83bb7f50564f7a46c4277d02eb931d27a25aa
Qmqmmyay <your.email@example.com>
2025-09-16 13:59:22 +0700

    Setup GitHub Actions workflow for e-plantShopping deployment

--- Final Project/e-plantShopping/vite.config.js
+++ Final Project/e-plantShopping/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Developing-Front-end-Apps-with-React/",
+  base: "/Developing-Front-end-Apps-with-React/Final Project/e-plantShopping/",
   plugins: [react()],
 })
